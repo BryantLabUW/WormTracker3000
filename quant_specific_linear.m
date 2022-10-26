5b0a96c4cab5ea1a68635fb815b7f28e242902ec
astrasb <astra.bryant@gmail.com>
2022-10-25 20:13:47 -0700

    Update quant_specific_linear.m
    Fix array orientation error

--- quant_specific_linear.m
+++ quant_specific_linear.m
@@ -13,13 +13,13 @@ finalgradientval = arrayfun(@(x,y) xvals(x,y), Indices, 1:info.numworms);
 % values equal to Gradient(max) and Gradient(min). This adjustment fixes an
 % issue if the gradient doesn't extend linearly to the very edges of the
 % assay plate, but instead include plateaus at min and max values.
-finalgradientval(finalgradientval>info.gradient.max) = info.gradient.max(finalgradientval>info.gradient.max);
-finalgradientval(finalgradientval<info.gradient.min) = info.gradient.min(finalgradientval<info.gradient.min);
+finalgradientval(finalgradientval>info.gradient.max') = info.gradient.max(finalgradientval>info.gradient.max');
+finalgradientval(finalgradientval<info.gradient.min') = info.gradient.min(finalgradientval<info.gradient.min');
 
 % Where did the worm start (with floor/ceiling)?
 startgradientval=xvals(1,:);
-startgradientval(startgradientval>info.gradient.max) = info.gradient.max(startgradientval>info.gradient.max);
-startgradientval(startgradientval<info.gradient.min) = info.gradient.max(startgradientval<info.gradient.min);
+startgradientval(startgradientval>info.gradient.max') = info.gradient.max(startgradientval>info.gradient.max');
+startgradientval(startgradientval<info.gradient.min') = info.gradient.max(startgradientval<info.gradient.min');
 
 
 % Calculate change location of worm along the gradient
